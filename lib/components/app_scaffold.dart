af15a49c52a8e75a07f4b792c96184517707b029
Vanilla <vanilla@nya.gg>
2024-12-21 20:16:52 +0100

    Refactor app scaffold

---
+++ lib/components/app_scaffold.dart
@@ -0,0 +1,73 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AppScaffold extends StatelessWidget {
+  final Widget body;
+
+  const AppScaffold({super.key, required this.body});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(context),
+      body: body,
+    );
+  }
+
+  // Builds the app bar
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      foregroundColor: Theme.of(context).colorScheme.onPrimary,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          buildLeadingBar(context),
+          buildTrailingBar(context),
+        ],
+      ),
+      actions: [
+        buildLogoutButton(),
+      ],
+    );
+  }
+
+  // Builds the leading part of the app bar
+  Row buildLeadingBar(BuildContext context) {
+    return Row(
+      children: [
+        const Icon(
+          Icons.rocket_launch,
+          size: 32,
+        ),
+        const SizedBox(width: 8),
+        Text(
+          'Cockpit',
+          style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
+        ),
+      ],
+    );
+  }
+
+  // Builds the trailing part of the app bar
+  Text buildTrailingBar(BuildContext context) {
+    return Text(
+      'Logged in as ${FirebaseAuth.instance.currentUser?.displayName ?? 'User'}',
+      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+            color: Theme.of(context).colorScheme.onPrimary,
+          ),
+    );
+  }
+
+  // Builds the logout button
+  IconButton buildLogoutButton() {
+    return IconButton(
+      onPressed: () {
+        FirebaseAuth.instance.signOut();
+      },
+      icon: const Icon(Icons.logout),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cockpit/components/app_scaffold.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,46 +6,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        foregroundColor: Theme.of(context).colorScheme.onPrimary,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              children: [
-                const Icon(
-                  Icons.rocket_launch,
-                  size: 32,
-                ),
-                const SizedBox(width: 8),
-                Text(
-                  'Cockpit',
-                  style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                        color: Theme.of(context).colorScheme.onPrimary,
-                      ),
-                ),
-              ],
-            ),
-            Text(
-              'Logged in as ${FirebaseAuth.instance.currentUser?.displayName ?? 'User'}',
-              style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                    color: Theme.of(context).colorScheme.onPrimary,
-                  ),
-            ),
-          ],
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            icon: const Icon(Icons.logout),
-          ),
-        ],
-      ),
-      body: const Center(
+    return const AppScaffold(
+      body: Center(
         child: Text('Home'),
       ),
     );
